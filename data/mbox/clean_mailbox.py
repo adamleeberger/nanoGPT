0e5e8549bcc7986c5228c8256664e86e12ae4bf1
Adam Berger <adam.berger@gmail.com>
2023-06-08 10:04:14 -0400

    segregate sent/received

--- data/mbox/clean_mailbox.py
+++ data/mbox/clean_mailbox.py
@@ -12,22 +12,32 @@ import argparse
 
 EMAIL_SEPARATOR = "**EOM**"
 
-parser = argparse.ArgumentParser(description='pre-prepare mbox file')
-parser.add_argument('-f', '--file', required=True, help='File path')
-parser.add_argument('-n', '--messages', type=int, default=sys.maxsize, help='number of messages to process (default: all)')
-args = parser.parse_args()
-file_path = args.file
-n_messages_to_process = args.messages or 999999999 
-
-mbox_file = file_path;
-
-multipart_messages = 0; # running counter 
+def canonicalize(email_address):
+    # joe.sm.ith@gmail.com -> joesmith@gmail.com
+    email_address = email_address.lower()
+    idx = email_address.find("@gmail.com")
+    if idx > 0:
+        prefix = email_address[0:idx]
+        prefix = prefix.replace(".", "")
+        email_address = prefix + "@gmail.com" 
+    return email_address
 
 # extract text from HTML
 def extract_text(html):
     soup = BeautifulSoup(html, 'html.parser')
     text = soup.get_text(separator=' ')
     return text
+
+parser = argparse.ArgumentParser(description='pre-prepare mbox file')
+parser.add_argument('-f', '--file', required=True, help='File path')
+parser.add_argument('-e', '--email_address', required=True, help='my email address')
+parser.add_argument('-n', '--messages', type=int, default=sys.maxsize, help='number of messages to process (default: all)')
+args = parser.parse_args()
+mbox_file = args.file
+n_messages_to_process = args.messages or 999999999 
+my_email_address = canonicalize(args.email_address)
+
+multipart_messages = 0; # running counter 
 
 # decode to utf-8 and remove any lines that can't be decoded
 def decode(m):
@@ -36,21 +46,41 @@ def decode(m):
  
 print ("Opening mbox file " + mbox_file, file=sys.stderr);    
 mbox = mailbox.mbox(mbox_file)
+
+output_prefix = mbox_file + ".cleaned"
+
 total_messages = len(mbox)
 quota = min (total_messages, n_messages_to_process)
 print (f"Found {total_messages} messages.", file=sys.stderr)
 print (f"Will process {quota} messages", file=sys.stderr) 
+print (f"Writing to {output_prefix}.sent and {output_prefix}.received", file=sys.stderr)
 
-# Iterate over each email in the mbox file
-n = 0;
+# segregate messages into sent and received. Some messages will be in both, of course. 
+sent_messages_output_file     = output_prefix + ".sent"
+received_messages_output_file = output_prefix + ".received"
+fs = open(sent_messages_output_file, "w")
+fr = open(sent_messages_output_file, "w")
+
+n = 0
+n_sent = 0
+n_received = 0;
 for message in mbox:
     n += 1
-    print (EMAIL_SEPARATOR + "\n") 
-    if (n % 100 ==0 or n == total_messages ):
-        print (f"Processed {n} messages; ; found {multipart_messages} multipart messages.", file=sys.stderr)
+    if (n % 100 ==0 or n==total_messages):
+        print (f"Processed {n} messages: {n_sent} sent messages, {n_received} received messages, {multipart_messages} multipart messages.", file=sys.stderr)
 
     if (n >= quota):
         break;
+
+    f = canonicalize(str(message['From']))
+    t = canonicalize(str(message['To']))
+    cc = canonicalize(str(message['Cc']))
+    bcc = canonicalize(str(message['Bcc']))
+
+    belongs_in_sent_folder = True if my_email_address in str(f) else False
+    belongs_in_received_folder = True if my_email_address in str(t) or my_email_address in str(cc) or my_email_address in str(bcc) else False
+    if (not belongs_in_received_folder and not belongs_in_sent_folder):
+            print (f"From: {f}; To: {t};", file=sys.stderr)
 
     # Extract the text content
     if message.is_multipart():
@@ -76,5 +106,13 @@ for message in mbox:
     lines = [line for line in lines if line.strip() != ""]
     text =  '\n'.join(lines)
 
-    # Print the processed text
-    print(text)
+    # Print the processed text to the appropriate file(s)
+    if belongs_in_sent_folder:
+        fs.write(text + EMAIL_SEPARATOR)
+        n_sent += 1
+    if belongs_in_received_folder:
+        fr.write(text + EMAIL_SEPARATOR)
+        n_received += 1
+
+fr.close()
+fs.close()
